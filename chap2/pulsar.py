483734fcc71af1946d73a099443567a99b322f98
eeuoo <lee512joo@gmail.com>
2019-11-15 23:25:17 +0900

    2019-11-15

--- chap2/pulsar.py
+++ chap2/pulsar.py
@@ -17,3 +17,20 @@ def load_pulsar_dataset():
     global data, input_cnt, output_cnt
     input_cnt, output_cnt = 8, 1
     data = np.asarray(rows, dtype = 'float32')
+
+
+def forward_postproc(output, y) :
+    entropy = sigmoid_cross_entropy_with_logits(y, output)
+    loss = np.mean(entropy)
+    return loss, [y, output, entropy]
+
+def backprop_postproc(G_loss, aux) :
+    y, output, entropy = aux
+
+    g_loss_entropy = 1.0 / np.prod(entropy.shape)
+    g_entropy_output = sigmoid_cross_entropy_with_logits_derv(y, output)
+
+    G_entropy = g_loss_entropy * G_loss
+    G_output = g_entropy_output * G_entropy
+
+    return G_output
