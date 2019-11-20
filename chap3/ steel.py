8ee416a2b92db24d39f48f247fc400f4c40d6415
eeuoo <lee512joo@gmail.com>
2019-11-20 23:55:25 +0900

    2019-11-20

--- chap3/ steel.py
+++ chap3/ steel.py
@@ -22,3 +22,13 @@ def forward_postproc(output, y) :
     loss = np.mean(entropy)
     return loss, [y, output, entropy]
 
+def backprop_postproc(G_loss, aux) :
+    y, output, entropy = aux
+
+    g_loss_entropy = 1.0 / np.prod(entropy.shape)
+    g_entropy_output = softmax_cross_entropy_with_logits_derv(y, output)
+
+    G_entropy = g_entropy_output * G_loss
+    G_output = g_entropy_output * G_entropy
+
+    return G_output
