972db4133b8a9f584299554087c1d85cec36dcd8
eeuoo <lee512joo@gmail.com>
2019-12-03 23:59:31 +0900

    2019-12-03

--- deep_learning_from_scratch_2/ch01/forward_net.py
+++ deep_learning_from_scratch_2/ch01/forward_net.py
@@ -15,3 +15,31 @@ class Affine:
         W, b = self.params
         out = np.matmul(x, W) + b
         return out
+
+class TwoLayerNet :
+    def __init__(self, input_size, hidden_size, output_size):
+        I, H, O = input_size, hidden_size, output_size
+
+        # 가중치와 편향 초기화
+        w1 = np.random.randn(I, H)
+        b1 = np.random.randn(H)
+        w2 = np.random.randn(H, O)
+        b2 = np.random.randn(O)
+
+        # 계층 생성
+        self.layers = [
+            Affine(W1, b1),
+            Sigmoid(),
+            Affine(W2, b2)
+        ]
+
+        # 모든 가중치를 리스트에 모은다.
+        self.params = []
+
+        for layer in self.layers:
+            self.params += layer.params
+        
+    def predict(self, x):
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
