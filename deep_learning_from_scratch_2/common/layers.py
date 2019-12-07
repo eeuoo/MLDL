b0359374c88aa43caed482f23261c69da4ca3ffd
eeuoo <lee512joo@gmail.com>
2019-12-07 13:11:34 +0900

    2019-12-07

--- deep_learning_from_scratch_2/common/layers.py
+++ deep_learning_from_scratch_2/common/layers.py
@@ -37,3 +37,28 @@ class Sigmoid :
         dx = dout * (1.0 - self.out) * self.out
 
         return dx
+
+
+class Affine :
+    def __init__(self, W, b) :
+        self.params = [W, b]
+        self.grads = [np.zeros_like(W), np.zeros_like(b)]
+        self.x = None
+
+    def forward(self, x) :
+        W, b = self.params
+        out = np.matmul(x, W) + b
+        self.x = x
+
+        return out
+
+    def backward(self, dout) :
+        W, b = self.params
+        dx = np.matmul(dout, W.T)
+        dW = np.matmul(self.x.T, dout)
+        db = np.sum(dout, axis=0)
+
+        self.grads[0][...] = dW
+        self.grads[1][...] = db
+
+        return dx
