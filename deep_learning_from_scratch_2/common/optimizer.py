228043d8be20210c40a9004bc55e8f1b3d16eae1
eeuoo <lee512joo@gmail.com>
2019-12-08 13:17:36 +0900

    2019-12-08

---
+++ deep_learning_from_scratch_2/common/optimizer.py
@@ -0,0 +1,9 @@
+
+# 확률적경사하강법 (Stochastic Gradient Descent)
+class SGD :    
+    def __init__(self, lr = 0.01) :
+        self.lr = lr
+
+    def update(self, params, grads) :
+        for i in range( len(params) ) :
+            params[i] -= self.lr * grads[i]
