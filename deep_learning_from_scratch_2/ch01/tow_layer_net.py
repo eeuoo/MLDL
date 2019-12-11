53a8d42eb42e422b4b3e727bbce7489dd8b742fc
eeuoo <lee512joo@gmail.com>
2019-12-11 13:58:39 +0900

    2019-12-11

--- deep_learning_from_scratch_2/ch01/tow_layer_net.py
+++ deep_learning_from_scratch_2/ch01/tow_layer_net.py
@@ -27,3 +27,22 @@ class TwoLayerNet :
         for layer in self.layers :
             self.params += layer.params
             self.grads += layer.grads
+
+    def predict(self, x) :
+        for layer in self.layers :
+            x = layer.forward(x)
+        
+        return x
+
+    def forward(self, x, t) :
+        score = self.predict(x)
+        loss = self.loss_layer.forward(score, t)
+
+        return loss
+
+    def backward(self, dout = 1) :
+        dout = self.loss_layer.backward(dout)
+        for layer in reversed(self.layers) :
+            dout = layer.backward(dout)
+
+        return dout
