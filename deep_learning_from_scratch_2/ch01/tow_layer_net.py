a86ac6e5c53563955f8fef3430515cf8e4b21720
eeuoo <lee512joo@gmail.com>
2019-12-11 13:50:27 +0900

    2019-12-11

--- deep_learning_from_scratch_2/ch01/tow_layer_net.py
+++ deep_learning_from_scratch_2/ch01/tow_layer_net.py
@@ -8,9 +8,9 @@ class TwoLayerNet :
         I, H, O = input_size, hidden_size, output_size
 
         # 가중치와 편향 초기화
-        W1 = 0.01 * np.random.randn(I, H)   
+        W1 = 0.01 * np.random.randn(I, H)   # 가중치 작은 무작위 값으로 초기화
         b1 = np.zeros(H)
-        W2 = 0.01 * np.random.randn(H, O)
+        W2 = 0.01 * np.random.randn(H, O)   # 가중치 작은 무작위 값으로 초기화
         b2 = np.zeros(0)
 
         # 계층 생성
@@ -20,7 +20,7 @@ class TwoLayerNet :
             Affine(W2, b2)
         ]
 
-        self.loss_layer = SoftmaxWithLoss()
+        self.loss_layer = SoftmaxWithLoss()  # 다른 계층과 다르게 취급하여 layers 리스트가 아닌, loss_layer 인스턴스 변수에 별도 저장한다
 
         # 모든 가중치화 기울기를 리스트에 모은다
         self.params, self.grads = [], []
