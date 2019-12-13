bb287e95b7c9eb6e8948a3096b5670c501b17ed0
eeuoo <lee512joo@gmail.com>
2019-12-13 14:43:04 +0900

    2019-12-13

--- deep_learning_from_scratch_2/common/functions.py
+++ deep_learning_from_scratch_2/common/functions.py
@@ -10,10 +10,21 @@ def softmax(x) :
     if x.ndim == 2 :
         x = x - x.max(axis=1, keepdimx=True)
         x = np.exp(x)
-        x /= x.sum(axis=1, keepdims=Ture)
+        x /= x.sum(axis=1, keepdims=True)
     elif x.dim == 1 :
         x = x - np.max(x)
         x = np.exp(x) / np.sum(np.exp(x))
 
     return x
 
+def cross_entropy_error(y, t) :
+    if y.ndim == 1 :
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    
+    if t.size == y.size :
+        t = t.argmax(axis=1)
+    
+    batch_size = y.shape[0]
+
+    return -np.sum( np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
