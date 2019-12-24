9e2ab12f3cb442924e7c3dfb7af567e4d3f5e550
eeuoo <lee512joo@gmail.com>
2019-12-24 18:38:32 +0900

    2019-12-24

--- deep_learning_from_scratch_2/common/util.py
+++ deep_learning_from_scratch_2/common/util.py
@@ -255,3 +255,38 @@ def eval_seq2seq(model, question, correct, id_to_char, verbos = False, is_revers
         print('---')
 
     return 1 if guess == correct else 0
+
+
+def analogy(a, b, c, word_to_id, id_to_word, word_matrix, top = 5, answer = None) :
+    for word in (a, b, c) :
+        if word not in word_to_id :
+            print('%s(을)를 찾을 수 없습니다.' % word)
+            return
+
+    print('\n[analogy] ' + a + ':' + b + ' = ' + c + ':?')
+    a_vec, b_vec, c_vec = word_matrix[word_to_id[a]], word_matrix[word_to_id[b]], word_matrix[word_to_id[c]]
+    query_vec = b_vec - a_vec + c_vec
+    query_vec = normalize(query_vec)
+
+    similarity = np.dot(word_matrix, query_vec)
+
+    if answer is not None :
+        print('==>' + answer + ':' + str(np.dot(word_matrix[word_to_id[answer]], query_vec)))
+
+    count = 0
+
+    for i in (-1 * similarity).argsort() :
+        if np.isnan(similarity[i]) :
+            continue
+        
+        if id_to_word[i] in (a, b, c) :
+            continue
+        
+        print(' {0}: {1}'.format(id_to_word[i], simliarity[i]))
+    
+        count += 1
+        if count >= top :
+            return
+
+
+def 
