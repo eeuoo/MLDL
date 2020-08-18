9f167b5827b9a60f4998746dc0654dae90d43ec4
eeuoo <lee512joo@gmail.com>
2020-08-18 21:28:28 +0900

    2020-08-18

--- deep_learning_from_scratch_2/common/time_layer.py
+++ deep_learning_from_scratch_2/common/time_layer.py
@@ -289,3 +289,323 @@ class TimeAffine:
         self.grads[1][...] = db
 
         return dx
+
+
+class TimeSoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.cache = None
+        self.ignore_label = -1
+
+    def forward(self, xs, ts):
+        N, T, V = xs.shape
+
+        if ts.ndim == 3:  # 정답 레이블이 원핫 벡터인 경우
+            ts = ts.argmax(axis=2)
+
+        mask = (ts != self.ignore_label)
+
+        # 배치용과 시계열용을 정리(reshape)
+        xs = xs.reshape(N * T, V)
+        ts = ts.reshape(N * T)
+        mask = mask.reshape(N * T)
+
+        ys = softmax(xs)
+        ls = np.log(ys[np.arange(N * T), ts])
+        ls *= mask  # ignore_label에 해당하는 데이터는 손실을 0으로 설정
+        loss = -np.sum(ls)
+        loss /= mask.sum()
+
+        self.cache = (ts, ys, mask, (N, T, V))
+        return loss
+
+    def backward(self, dout=1):
+        ts, ys, mask, (N, T, V) = self.cache
+
+        dx = ys
+        dx[np.arange(N * T), ts] -= 1
+        dx *= dout
+        dx /= mask.sum()
+        dx *= mask[:, np.newaxis]  # ignore_labelㅇㅔ 해당하는 데이터는 기울기를 0으로 설정
+
+        dx = dx.reshape((N, T, V))
+
+        return dx
+
+
+class TimeDropout:
+    def __init__(self, dropout_ratio=0.5):
+        self.params, self.grads = [], []
+        self.dropout_ratio = dropout_ratio
+        self.mask = None
+        self.train_flg = True
+
+    def forward(self, xs):
+        if self.train_flg:
+            flg = np.random.rand(*xs.shape) > self.dropout_ratio
+            scale = 1 / (1.0 - self.dropout_ratio)
+            self.mask = flg.astype(np.float32) * scale
+
+            return xs * self.mask
+        else:
+            return xs
+
+    def backward(self, dout):
+        return dout * self.mask
+
+
+class TimeBiLSTM:
+    def __init__(self, Wx1, Wh1, b1,
+                 Wx2, Wh2, b2, stateful=False):
+        self.forward_lstm = TimeLSTM(Wx1, Wh1, b1, stateful)
+        self.backward_lstm = TimeLSTM(Wx2, Wh2, b2, stateful)
+        self.params = self.forward_lstm.params + self.backward_lstm.params
+        self.grads = self.forward_lstm.grads + self.backward_lstm.grads
+
+    def forward(self, xs):
+        o1 = self.forward_lstm.forward(xs)
+        o2 = self.backward_lstm.forward(xs[:, ::-1])
+        o2 = o2[:, ::-1]
+
+        out = np.concatenate((o1, o2), axis=2)
+        return out
+
+    def backward(self, dhs):
+        H = dhs.shape[2] // 2
+        do1 = dhs[:, :, :H]
+        do2 = dhs[:, :, H:]
+
+        dxs1 = self.forward_lstm.backward(do1)
+        do2 = do2[:, ::-1]
+        dxs2 = self.backward_lstm.backward(do2)
+        dxs2 = dxs2[:, ::-1]
+        dxs = dxs1 + dxs2
+        return dxs
+
+# ====================================================================== #
+# 이 아래의 계층들은 책에서 설명하지 않았거나
+# 처리 속도보다는 쉽게 이해할 수 있도록 구현했습니다.
+#
+# TimeSigmoidWithLoss: 시계열 데이터용 시그모이드 + 손실 계층
+# GRU: GRU 계층
+# TimeGRU: 시계열 데이터용 GRU 계층
+# BiTimeLSTM: 양방향 LSTM 계층
+# Simple_TimeSoftmaxWithLoss：간단한 TimeSoftmaxWithLoss 계층의 구현
+# Simple_TimeAffine: 간단한 TimeAffine 계층의 구현
+# ====================================================================== #
+
+
+class TimeSigmoidWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.xs_shape = None
+        self.layers = None
+
+    def forward(self, xs, ts):
+        N, T = xs.shape
+        self.xs_shape = xs.shape
+
+        self.layers = []
+        loss = 0
+
+        for t in range(T):
+            layer = SigmoidWithLoss()
+            loss += layer.forward(xs[:, t], ts[:, t])
+            self.layers.append(layer)
+
+        return loss / T
+
+    def backward(self, dout=1):
+        N, T = self.xs_shape
+        dxs = np.empty(self.xs_shape, dtype='f')
+
+        dout *= 1/T
+        for t in range(T):
+            layer = self.layers[t]
+            dxs[:, t] = layer.backward(dout)
+
+        return dxs
+
+
+class GRU:
+    def __init__(self, Wx, Wh):
+        '''
+        Parameters
+        ----------
+        Wx: 입력 x에 대한 가중치 매개변수(3개 분의 가중치가 담겨 있음)
+        Wh: 은닉 상태 h에 대한 가중치 매개변수(3개 분의 가중치가 담겨 있음)
+        '''
+        self.Wx, self.Wh = Wx, Wh
+        self.dWx, self.dWh = None, None
+        self.cache = None
+
+    def forward(self, x, h_prev):
+        H, H3 = self.Wh.shape
+        Wxz, Wxr, Wx = self.Wx[:, :H], self.Wx[:, H:2 * H], self.Wx[:, 2 * H:]
+        Whz, Whr, Wh = self.Wh[:, :H], self.Wh[:, H:2 * H], self.Wh[:, 2 * H:]
+
+        z = sigmoid(np.dot(x, Wxz) + np.dot(h_prev, Whz))
+        r = sigmoid(np.dot(x, Wxr) + np.dot(h_prev, Whr))
+        h_hat = np.tanh(np.dot(x, Wx) + np.dot(r*h_prev, Wh))
+        h_next = (1-z) * h_prev + z * h_hat
+
+        self.cache = (x, h_prev, z, r, h_hat)
+
+        return h_next
+
+    def backward(self, dh_next):
+        H, H3 = self.Wh.shape
+        Wxz, Wxr, Wx = self.Wx[:, :H], self.Wx[:, H:2 * H], self.Wx[:, 2 * H:]
+        Whz, Whr, Wh = self.Wh[:, :H], self.Wh[:, H:2 * H], self.Wh[:, 2 * H:]
+        x, h_prev, z, r, h_hat = self.cache
+
+        dh_hat =dh_next * z
+        dh_prev = dh_next * (1-z)
+
+        # tanh
+        dt = dh_hat * (1 - h_hat ** 2)
+        dWh = np.dot((r * h_prev).T, dt)
+        dhr = np.dot(dt, Wh.T)
+        dWx = np.dot(x.T, dt)
+        dx = np.dot(dt, Wx.T)
+        dh_prev += r * dhr
+
+        # update gate(z)
+        dz = dh_next * h_hat - dh_next * h_prev
+        dt = dz * z * (1-z)
+        dWhz = np.dot(h_prev.T, dt)
+        dh_prev += np.dot(dt, Whz.T)
+        dWxz = np.dot(x.T, dt)
+        dx += np.dot(dt, Wxz.T)
+
+        # rest gate(r)
+        dr = dhr * h_prev
+        dt = dr * r * (1-r)
+        dWhr = np.dot(h_prev.T, dt)
+        dh_prev += np.dot(dt, Whr.T)
+        dWxr = np.dot(x.T, dt)
+        dx += np.dot(dt, Wxr.T)
+
+        self.dWx = np.hstack((dWxz, dWxr, dWx))
+        self.dWh = np.hstack((dWhz, dWhr, dWh))
+
+        return dx, dh_prev
+
+
+class TimeGRU:
+    def __init__(self, Wx, Wh, stateful=False):
+        self.Wx, self.Wh = Wx, Wh
+        selfdWx, self.dWh = None, None
+        self.layers = None
+        self.h, self.dh = None, None
+        self.stateful = stateful
+
+    def forward(self, xs):
+        N, T, D = xs.shape
+        H, H3 = self.Wh.shape
+
+        self.layers = []
+        hs = np.empty((N, T, H), dtype='f')
+
+        if not self.stateful or self.h is None:
+            self.h = np.zeros((N, H), dtype='f')
+
+        for t in range(T):
+            layer = GRU(self.Wx, self.Wh)
+            self.h = layer.forward(xs[:, t, :], self.h)
+            hs[:, t, :] = self.h
+            self.layers.append(layer)
+
+        return hs
+
+    def backward(self, dhs):
+        N, T, H = dhs.shape
+        D = self.Wx.shape[0]
+
+        dxs = np.empty((N, T, D), dtype='f')
+        self.dWx, self.dWh = 0, 0
+
+        dh = 0
+        for t in reversed(range(T)):
+            layer = self.layers[t]
+            dx, dh = layer.backward(dhs[:, t, :] + dh)
+
+            dxs[:, t, :] = dx
+            self.dWx += layer.dWx
+            self.dWh += layer.dWh
+
+        self.dh = dh
+        return dxs
+
+    def set_state(self, h):
+        self.h = h
+
+    def reset_state(self):
+        self.h = None
+
+
+class Simple_TimeSoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.cache = None
+
+    def forward(self, xs, ts):
+        N, T, V = xs.shape
+        layers = []
+        loss = 0
+
+        for t in range(T):
+            layer = SoftmaxWithLoss()
+            loss += layer.forward(xs[:, t, :], ts[:, t])
+            layers.append(layer)
+        loss /= T
+
+        self.cache = (layers, xs)
+        return loss
+
+    def backward(self, dout=1):
+        layers, xs = self.cache
+        N, T, V = xs.shape
+        dxs = np.empty(xs.shape, dtype='f')
+
+        dout *= 1/T
+        for t in range(T):
+            layer = layers[t]
+            dxs[:, t, :] = layer.backward(dout)
+
+        return dxs
+
+
+class Simple_TimeAffine:
+    def __init__(self, W, b):
+        self.W, self.b = W, b
+        self.dW, self.db = None, None
+        self.layers = None
+
+    def forward(self, xs):
+        N, T, D = xs.shape
+        D, M = self.W.shape
+
+        self.layers = []
+        out = np.empty((N, T, M), dtype='f')
+        for t in range(T):
+            layer = Affine(self.W, self.b)
+            out[:, t, :] = layer.forward(xs[:, t, :])
+            self.layers.append(layer)
+
+        return out
+
+    def backward(self, dout):
+        N, T, M = dout.shape
+        D, M = self.W.shape
+
+        dxs = np.empty((N, T, D), dtype='f')
+        self.dW, self.db = 0, 0
+        for t in range(T):
+            layer = self.layers[t]
+            dxs[:, t, :] = layer.backward(dout[:, t, :])
+
+            self.dW += layer.dW
+            self.db += layer.db
+
+        return dxs
