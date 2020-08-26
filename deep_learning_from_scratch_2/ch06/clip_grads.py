9ace399585551894851c5d6c6ee07499e1cb68d3
eeuoo <lee512joo@gmail.com>
2020-08-26 22:37:46 +0900

    2020-08-26

--- deep_learning_from_scratch_2/ch06/clip_grads.py
+++ deep_learning_from_scratch_2/ch06/clip_grads.py
@@ -16,3 +16,13 @@ def clip_grads(grads, max_norm):
     if rate < 1:
         for grad in grads:
             grad *= rate
+
+
+print('before:', dW1.flatten())
+clip_grads(grads, max_norm)
+print('after:', dW1.flatten())
+
+
+# 결과
+# before: [2.1761174  7.384534   4.28052605 1.84073141 0.50402541 4.35053292 1.56027608 6.06908582 7.41833507]
+# after: [0.48272062 1.63808572 0.94953434 0.4083231  0.11180622 0.96506372 0.34611066 1.34628438 1.64558369]
