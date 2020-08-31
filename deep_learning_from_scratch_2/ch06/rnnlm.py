d4e1248af78b4ef5d59f8e598fc048fafc67eacd
eeuoo <lee512joo@gmail.com>
2020-08-31 22:43:55 +0900

    2020-08-31

---
+++ deep_learning_from_scratch_2/ch06/rnnlm.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.append('..')
+from common.time_layers import *
+from common.base_model import BaseModel
+
+class Rnnlm(BaseModel):
+    def __init__(self, vocab_size=10000, wordvec_size=100, hidden_size=100):
+        rn = np.random.rnadn
+
+        # 가중치 초기화 
+        embed_W = (rn(V, D) / 100).astype('f')
+        lstm_Wx = (rn(D, 4 * H) / np.sqrt(D)).astype('f')
+        lstm_Wh = (rn(H, 4 * H) / np.sqrt(H)).astype('f')
+        lstm_b = np.zeros(4 * H).astype('f')
+        affine_W = (rn(H, V) / np.sqrt(H)).astype('f')
+        affine_b = np.zeros(V).astype('f')
+
+        # 계층 생성
+        self.layers = [
+            TimeEmbedding(embed_W),
+            TimeLSTM(lstm_Wx, lstm_Wh, lstm_b, stateful=True),
+            TimeAffine(affine_W, affine_b)
+        ]
+        self.loss_layer = TimeSoftWithLoss()
+        self.lstm_layer = self.layers[1]
+
+       
