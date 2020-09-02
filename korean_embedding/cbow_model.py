443da86b0a09624bdfc2b51f511ed19a79e0784d
lhjoo <lee512joo@gmail.com>
2020-09-02 11:37:07 +0900

    2020-09-02

--- korean_embedding/cbow_model.py
+++ korean_embedding/cbow_model.py
@@ -56,3 +56,37 @@ class CBoWModel(object):
                     total_count += 1
 
         return words_count, total_count
+
+
+    def load_or_construct_weighted_embedding(self, embedding_fname, embedding_method,
+                                             embedding_corpus_fname, a=0.0001) :
+        dictionary = {}
+
+        if os.path.exists(embedding_fname + "-weighted") :
+            # load weighted word embeddings
+            with open(embedding_fname + "-weighted", "r") as f2 :
+                for line in f2 :
+                    word, weighted_vector = line.strip().split("\u241E")
+                    weighted_vector = [float(el) for el in weighted_vector.split()]
+                    dictionary[word] = weighted_vector
+
+        else :
+            # load pretrained word embeddngs
+            words, vecs = self.load_word_embeddings(embedding_fname, embedding_method)
+
+            # compute word frequency
+            words_count, total_word_count = self.compute_word_frequency(embedding_corpus_fname)
+
+            # construct weighted word embeddings
+            with open(embedding_fname + '-weighted', 'w') as f3 :
+                for word, vec in zip(words, vecs) :
+                    if word in words_count.keys() :
+                        word_prob = words_count[word] / total_word_count
+                    else :
+                        word_prob = 0.0
+
+                    weighted_vector = (a / (word_prob + a)) * np.asarray(vec)
+                    dictionary[word] = weighted_vector
+                    f3.writelines(word + "\u214E" + " ".join([str(el) for el in weighted_vector]) + "\n")
+
+        return dictionary
