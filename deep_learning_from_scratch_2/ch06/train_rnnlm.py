42fccff994ce69b02f8847b5b8501e4b47415629
eeuoo <lee512joo@gmail.com>
2020-09-15 22:14:42 +0900

    2020-09-15

--- deep_learning_from_scratch_2/ch06/train_rnnlm.py
+++ deep_learning_from_scratch_2/ch06/train_rnnlm.py
@@ -21,19 +21,24 @@ vocab_size = len(word_to_id)
 xs = corpus[:-1]
 tx = corpus[1:]
 
-# 모델 생성
+### 모델 생성
 model = Rnnlm(vocab_size, wordvec_size, hidden_size)
 optimizer = SGD(lr)
-trainer = RnnlmTrainer(model, optimizer)
+trainer = RnnlmTrainer(model, optimizer)  # RnnlmTrainer 클래스를 사용해 모델을 학습 
 
-# 기울기 클리핑을 적용하여 학습
-trainer.fit(xs, tx, max_epoch, batch_size, time_size, max_grad, eval_inerval=20)
+### 기울기 클리핑을 적용하여 학습
+# fit() 메서드는 모델의 기울기를 구해 모델의 매개변수를 갱신 
+# 이때 인수로 max_grad를 지정해 기울기 클리핑을 적용 (기울기 폭발 대책)
+# eval_interval=20 은 20번째 반복마다 퍼플렉서티를 평가하라는 뜻, 데이터가 크므로 모든 에폭에서 평가하지 않고 20번에서 평가
+trainer.fit(xs, tx, max_epoch, batch_size, time_size, max_grad, eval_interval=20)  
 trainer.plot(ylim=(0, 500))
 
-# 테스트 데이터로 평가
+### 테스트 데이터로 평가
+# 학습이 끝난 후 테스트 데이터를 사용해 퍼플렉서티를 평가 
+# 모델 상태(LSTM의 은닉 상태와 기억 셀)를 재설정하여 평가를 수행햇
 model.reset_state()
 ppl_test = eval_preplexity(model, corpus_test)
 print('테스트 퍼블렉서티: ', ppl_test)
 
-# 매개변수 저장
+### 매개변수 저장
 model.save_params()
