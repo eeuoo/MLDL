02d35a5bde46c29198cf3b519dd66717f2325631
eeuoo <lee512joo@gmail.com>
2020-09-21 21:41:14 +0900

    2020-09-21

--- deep_learning_from_scratch_2/ch07/rnnlm_gen.py
+++ deep_learning_from_scratch_2/ch07/rnnlm_gen.py
@@ -7,6 +7,11 @@ from ch06.better_rnnlm import BetterRnnlm
 
 class RnnlmGen(Rnnlm) :
     def generate(self, start_id, skip_ids=None, sample_size=100) :
+        # 문장 생성을 수행하는 메서드
+        # start_id : 최초로 주는 ID  
+        # sample_size : 샘플링하는 단어의 수 
+        # skip_ids : 단어 ID의 리스트, 이 리스트에 속하는 건 샘플링 되지 않도록 함. PTB 데이터셋에 있는 <unk>나 N 등, 전처리된 단어를 샘플링하지 않게 하는 용도로 사용
+        
         word_ids = [start_id]
 
         x = start_id
