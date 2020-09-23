33273c9ae66d15a1d81b85dc1662bded0b4d0135
eeuoo <lee512joo@gmail.com>
2020-09-23 22:13:37 +0900

    2020-09-23

--- deep_learning_from_scratch_2/ch07/generate_text.py
+++ deep_learning_from_scratch_2/ch07/generate_text.py
@@ -8,16 +8,16 @@ vocab_size = len(word_to_id)
 corpus_size = len(corpus)
 
 model = RnnlmGen()
-#model.load_params('../ch06/Rnnlm.pkl')
+#model.load_params('../ch06/Rnnlm.pkl') # 앞 장에서 학습을 끝낸 가중치 매개변수
 
 # 시작(start) 문자와 건너뜀(skip) 문자 설정
 start_word = 'you'
 start_id = word_to_id[start_word]
-skip_words = ['N', '<unk>', '$']
-skip_ids = [word_to_id[w] for w in skip_words]
+skip_words = ['N', '<unk>', '$'] # 샘플링하지 않을 단어 지정 
+skip_ids = [word_to_id[w] for w in skip_words] 
 
 # 문자 생성
-word_ids = model.generate(start_id, skip_ids)
-txt = ' '.join([id_to_word[i] for  i in word_ids])
+word_ids = model.generate(start_id, skip_ids) # 문장을 생성하는 generate() 는 단어 ID들을 배열 형태로 반환함 
+txt = ' '.join([id_to_word[i] for  i in word_ids]) 
 txt = replace(' <eos>', '.\n')
 print(txt)
