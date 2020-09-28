3acb2423d7743b93fe94df783941b04f2bca2af3
eeuoo <lee512joo@gmail.com>
2020-09-28 23:44:29 +0900

    2020-09-28

--- deep_learning_from_scratch_2/ch07/show_addition_dataset.py
+++ deep_learning_from_scratch_2/ch07/show_addition_dataset.py
@@ -2,7 +2,10 @@ import sys
 sys.path.append('..')
 from dataset import sequence
 
+# 문자 ID가 저장되어 있는 x_train, t_train
 (x_train, t_train) , (x_test, t_test) = sequnce.load_data('addition.txt', seed=1984)
+
+# 문자 ID와 문자의 대응 관계는 char_to_id, id_to_char 로 상호변환
 char_to_id, id_to_char = sequence.get_vocab()
 
 print(x_train.shape, t_train.shape)
@@ -13,3 +16,9 @@ print(t_train[0])
 
 print(''.join([id_to_char[c] for c in x_train[0]]))
 print(''.join([id_to_char[c] for c in t_train[0]]))
+
+
+# 정석대로라면 데이터 셋을 훈련용, 검증용, 테스트용으로 나눠 사용해야 한다. 
+# 훈련용 : 학습
+# 검증용 : 하이퍼파라미터 튜닝
+# 테스트 : 모델 성능 평가 
