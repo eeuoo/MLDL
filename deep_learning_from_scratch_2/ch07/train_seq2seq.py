60ee0b2949ed81980681d10917335faf5168c2a8
lhjoo <lee512joo@gmail.com>
2020-11-03 10:06:06 +0900

    2020-11-03

--- deep_learning_from_scratch_2/ch07/train_seq2seq.py
+++ deep_learning_from_scratch_2/ch07/train_seq2seq.py
@@ -26,3 +26,22 @@ max_grad = 5.0
 model = Seq2seq(vocab_size, wordvec_size, hideen_size)
 optimizer = Adam()
 trainer = Trainer(model, optimizer)
+
+acc_list = []
+
+for epoch in range(max_epoch) :
+    trainer.fit(x_train, t_train, max_epoch = 1,
+                batch_size = batch_size, max_grad = max_grad)
+
+    correct_num = 0
+
+    for i in range(len(x_test)) :
+        question, correct = x_test[[i]], t_test[[i]]
+        verbose = i < 10
+        correct_num += eval_seq2seq(model, question, correct,
+                                    id_to_char, verbose)
+
+    acc = float(correct_num) / len(x_test)
+    acc_list.append(acc)
+
+    print('검증 정확도 %.3f%%' % (acc * 100))
