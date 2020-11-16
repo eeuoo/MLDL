2ca6ebaccdf29c6fd220c9007ee0d4c73acb62c4
lhjoo <lee512joo@gmail.com>
2020-11-16 10:27:56 +0900

    2020-11-16

--- deep_learning_from_scratch_2/ch08/test_2.py
+++ deep_learning_from_scratch_2/ch08/test_2.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+### 가중합의 계산 ###
+
 N, T, H = 10, 5, 4
 hs = np.random.randn(N, T, H)
 a = np.random.randn(N, T)
@@ -14,3 +16,8 @@ print(t.shape)
 c = np.sum(t, axis=1)
 print(c.shape)
 # (10, 4)
+
+
+# a (N, T) -> ar (N, T, H) X hs (N, T, H) -> t (N, T, H) -> c (N, H)
+# repeat 노드를 사용해 a 복제, 원소별 곱을 계산한 다음 sum 노드로 합을 구함.
+# sum의 역전하는 repeat, repeat의 역전파는 sum
