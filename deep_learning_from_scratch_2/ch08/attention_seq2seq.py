a51a0e34eab0c436233c62297057911fb6e645b1
lhjoo <lee512joo@gmail.com>
2020-11-23 10:34:18 +0900

    2020-11-23

--- deep_learning_from_scratch_2/ch08/attention_seq2seq.py
+++ deep_learning_from_scratch_2/ch08/attention_seq2seq.py
@@ -8,7 +8,7 @@ from ch08.attention_layer import TimeAttention
 class AttentionEncoder(Encoder) :
     def forward(self, xs) :
         xs = self.embed.forward(xs)
-        hs = self.lstm.forward(xs)
+        hs = self.lstm.forward(xs) # 모든 은닉상태 반환 (앞 장에서는 마지막 상태 벡터만 반환했었음)
         return hs
 
     def backward(self, dhs) :
