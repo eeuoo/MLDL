0be0f2857f0faf072558ff4060d5b7407cec830c
lhjoo <lee512joo@gmail.com>
2020-11-24 10:37:49 +0900

    2020-11-24

--- deep_learning_from_scratch_2/ch08/attention_seq2seq.py
+++ deep_learning_from_scratch_2/ch08/attention_seq2seq.py
@@ -15,3 +15,41 @@ class AttentionEncoder(Encoder) :
         dout = self.lstm.backward(dhs)
         dout = self.embed.backward(dout)
         return dout
+
+
+class AttentionDecoder :
+    def __init__(self, vocab_size, wordvec_size, hidden_size) :
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        rn = np.random.randn
+        embed_W = (rn(V, D) / 100).astype('f')
+        lstm_Wx = (rn(D, 4 * H) / np.sqrt(D)).astype('f')
+        lstm_Wh = (rn(H, 4 * H) / np.sqrt(H)).astype('f')
+        lstm_b = np.zeros(4 * H).astype('f')
+        affine_W = (rn(2 * H, V) / np.sqrt(2 * H)).astype('f')
+        affine_b = np.zeros(V).astype('f')
+
+        self.embed = TimeEmbedding(embed_W)
+        self.lstm = TimeLSTM(lstm_Wx, lstm_Wh, lstm_b, sateful=True)
+        self.attention = TimeAttention()
+        self.affine = TimeAttention(affine_W, affine_b)
+        layers = [self.embed, self.lstm, self.attention, self.affine]
+
+        self.params, self.grads = [], []
+
+        for layer in layers :
+            self.params += layer.params
+            self.grads += layer.grads
+
+    def forward(self, xs, enc_hs) :
+        h = enc_hs[:, -1]
+        self.lstm.set_state(h)
+
+        out = self.embed.forward(xs)
+        dec_hs = self.lstm.forward(out)
+        c = self.attention.forward(enc_hs, dec_hs)
+        out = np.concatenate((c, dec_hs), axis = 2)
+        score = self.affine.forward(out)
+
+        return score
+
+
